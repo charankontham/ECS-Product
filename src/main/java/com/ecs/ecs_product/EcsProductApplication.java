407c398e1049004ec7fc00f5e25795ca04712f41
= <=>
2024-09-25 13:30:31 -0400

    Initial Commit From local

---
+++ src/main/java/com/ecs/ecs_product/EcsProductApplication.java
@@ -0,0 +1,13 @@
+package com.ecs.ecs_product;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EcsProductApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EcsProductApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ecs/ecs_product/EcsProductApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ecs.ecs_product;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EcsProductApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
